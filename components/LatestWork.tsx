88c266ea034f111ce48bcd11ceb8ee37002bffa3
Huzaifa Khan <huzaifakhan0856551@gmail.com>
2026-08-19 20:27:06 +0500

    add latest work section

--- components/LatestWork.tsx
+++ components/LatestWork.tsx
@@ -133,7 +133,7 @@ const ALL_PROJECTS = [
     title: 'Apex Vanguard V8',
     tags: ['Automotive', 'Render'],
     category: 'Animation',
-    image: 'https://unsplash.com/s/photos/luxury-car',
+    image: 'https://images.unsplash.com/photo-1614200179396-2bdb77ebf81b?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     size: 'large',
   },
   {
